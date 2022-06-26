504a7e09525cf965c056aa19345b8040df0c54a9
xxxOznogg <108197301+xxxOznogg@users.noreply.github.com>
2022-06-25 23:03:33 -0500

    Menu working
    The menu seems to be correctly starting and exiting the game. Bricks now spawn on a BoardManager, which requires its own Instance. Ball is no longer launching from the paddle when loading the scene, however. It seems to be something to do with m_Started.

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public class DeathZone : MonoBehaviour
 {
-    public MainManager Manager;
+    public GameObject BoardManager;
 
     private void OnCollisionEnter(Collision other)
     {
         Destroy(other.gameObject);
-        Manager.GameOver();
+        BoardManager.GetComponent<BoardManager>().GameOver();
     }
 }
